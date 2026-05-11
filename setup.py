f4af3f7c2bd1e11fe6ac2d8c59dd318af65fcae1
DGeriOE <g.dobovics@stud.uni-obuda.hu>
2026-05-12 01:24:16 +0200

    Base Package Initialization

---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import find_packages, setup
+
+package_name = 'ros2_course'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='geri',
+    maintainer_email='geri@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'hello = ros2_course.hello:main',
+            'turtlesim_controller = ros2_course.turtlesim_controller:main',
+            'psm_grasp = ros2_course.psm_grasp:main',
+            'psm_grasp2 = ros2_course.psm_grasp2:main',
+            'dummy_marker = ros2_course.dummy_marker:main',
+            'interactive_marker = ros2_course.interactive_marker:main',
+            'psm_interactive_grasp = ros2_course.psm_interactive_grasp:main',
+            
+        ],
+    },
+)
